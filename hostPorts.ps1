4b97a4874b698c4291133f99dec2e66d83779390
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-02-28 13:59:35 +0100

    Update hostPorts.ps1

--- hostPorts.ps1
+++ hostPorts.ps1
@@ -1,7 +1,7 @@
 # Ask the user to select the network to scan
 Write-Host "Running ipconfig..."
 $ipconfig = ipconfig /all
-$networks = $ipconfig | Select-String "IPv4 Address.*: (\d{1,3}\.\d{1,3}\.\d{1,3}\.)" | ForEach-Object { $_.Matches.Groups[1].Value } | Select-Object -Unique | Where-Object {$_ -ne '127.0.0.'}
+$networks = $ipconfig | Select-String "IPv4 Address.*: (\d{1,3}\.\d{1,3}\.\d{1,3}\.)" | ForEach-Object { $_.Matches.Groups[1].Value } | Where-Object { $_ -ne "127.0.0." } | Select-Object -Unique
 
 if ($networks.Count -eq 0) {
     Write-Host "No networks found to scan."
@@ -27,8 +27,8 @@ for ($i = 1; $i -le 255; $i++) {
     $result = $ping.Send($host, 1000)
     if ($result.Status -eq "Success") {
         Write-Host "Host $host is online."
-        # Scan for ports
-        Write-Host "Scanning ports on host $host..."
+        # Scan for TCP ports
+        Write-Host "Scanning TCP ports on host $host..."
         for ($port = 1; $port -le 65535; $port++) {
             $tcpClient = New-Object System.Net.Sockets.TcpClient
             $connect = $tcpClient.BeginConnect($host, $port, $null, $null)
@@ -36,8 +36,21 @@ for ($i = 1; $i -le 255; $i++) {
             if (!$wait) {
                 $tcpClient.Close()
             } else {
-                Write-Host "Port $port is open on host $host."
+                Write-Host "Port $port is open on host $host (TCP)."
                 $tcpClient.Close()
+            }
+        }
+        # Scan for UDP ports
+        Write-Host "Scanning UDP ports on host $host..."
+        for ($port = 1; $port -le 65535; $port++) {
+            $udpClient = New-Object System.Net.Sockets.UdpClient
+            $result = $udpClient.BeginReceive($null, $null)
+            $wait = $result.AsyncWaitHandle.WaitOne(100)
+            if (!$wait) {
+                $udpClient.Close()
+            } else {
+                Write-Host "Port $port is open on host $host (UDP)."
+                $udpClient.Close()
             }
         }
     } else {
